66f2e0cc1ec8fa6d429b8cc37511eb6a94554f29
FMN707 <muhammadamin071011@icloud.com>
2025-08-21 14:04:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let canvas = document.querySelector("canvas")
+
+
+new Chart(canvas,{
+type:"line",
+data: {
+labels: [
+"January",
+"Februarry",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"Octuber",
+"November",
+"December",
+],
+datasets: [
+{
+label: "Salary",
+data: [
+    1201, 1826, 1287, 1097, 7369, 9368, 9018, 8379, 7358, 7635, 7383, 1786.
+],
+BorderColor: "blue"
+
+
+
+
+
+}
+]
+
+
+
+
+
+
+
+
+}
+
+
+
+
+})
